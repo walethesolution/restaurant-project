7ecdbc064c9e3dadd61642492c4000c630d8b13f
Olawale Ayejuyone <olawaleayejuyone@gmail.com>
2022-06-07 14:34:00 -0400

    Test backend connection

--- front-end/src/utils/api.js
+++ front-end/src/utils/api.js
@@ -6,7 +6,9 @@ import formatReservationDate from "./format-reservation-date";
 import formatReservationTime from "./format-reservation-date";
 
 const API_BASE_URL =
-  process.env.REACT_APP_API_BASE_URL || "http://localhost:5001";
+  process.env.NODE_ENV === "production"
+    ? "https://restaurant-v1-backend.herokuapp.com"
+    : "http://localhost:5000";
 
 /**
  * Defines the default headers for these functions to work with `json-server`
