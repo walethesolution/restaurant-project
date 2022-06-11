723c0895ca150975b1a473e72de81de1ac3f55d9
Olawale Ayejuyone <olawaleayejuyone@gmail.com>
2022-06-11 14:40:40 -0400

    Add new test

--- back-end/knexfile.js
+++ back-end/knexfile.js
@@ -10,7 +10,7 @@ const path = require("path");
 const {
   DATABASE_URL = "postgresql://postgres@localhost/postgres",
   DATABASE_URL_DEVELOPMENT = "postgres://fsreodpc:f9khzanqv6iKd-e1VBMjZ3vmjuZ9s31K@fanny.db.elephantsql.com/fsreodpc",
-  DATABASE_URL_TEST = "postgres://lnlrgchb:M3VodHk_-VQZf7tBGFp_K93pNjFtbQCE@fanny.db.elephantsql.com/lnlrgchb",
+  DATABASE_URL_TEST = "postgresql://postgres@localhost/postgres",
   DATABASE_URL_PREVIEW = "postgresql://postgres@localhost/postgres",
   DEBUG,
 } = process.env;
